8d4c8f70d4f8e34328a20c2c1993bec19c0823b1
adammhaile <adammhaile@gmail.com>
2015-07-20 20:00:46 -0400

    improved snake

--- KiloDisplay/Snake.py
+++ KiloDisplay/Snake.py
@@ -2,15 +2,23 @@ import bibliopixel.colors as colors
 from bibliopixel.animation import BaseGameAnim
 import math
 from random import randint
+import bibliopixel.util as util
 
 class Snake(BaseGameAnim):
     def __init__(self, led, inputDev):
         super(Snake, self).__init__(led, inputDev)
-        self._speed = 0.5
+        self._growLen = 2
+        self._speed = 0.3
+        self._speedGrow = 0.1
+        self._level = 1
+        self._apCount = 0
         self._lastKeys = None
         self._apple = (-1, -1)
         self._gameOver = False
         self._gameOverCount = 0
+        self._levelUp = True
+        self._growCount = 0
+        self._directions = [(0,-1), (0,1), (1,0), (-1,0)]
         self.resetBody()
         self.placeApple()
 
@@ -20,31 +28,61 @@ class Snake(BaseGameAnim):
             self._led.set(x, y, colors.Red)
 
     def resetBody(self):
-        self._pos = (self._led.width/2,self._led.height/2)
-        self._body = [self._pos, (self._pos[0], self._pos[1]+1)]
-        self._dir = (0, -1)
+        dx, dy = self._dir = self._directions[0]#randint(0,3)]
+        dx, dy = dx*-1, dy*-1
+        x, y = self._pos = (self._led.width/2,self._led.height/2)
+        self._body = []
+        for i in range(self._growLen):
+            self._body.append(((x+(i*dx)),(y+(i*dy))))
+        self._newDir = None
 
     def gameOver(self):
         self._gameOver = True
         self._gameOverCount = 0
+        self._levelUp = True
+        self._level = 1
+        self._speed = 0.3
+
+    def nextLevel(self):
+        self._apCount = 0
+        self._levelUp = True
+        self._level += 1
+        self._speed += self._speedGrow
+        self.resetBody()
 
     def move(self):
-        d = tuple(x * y for x,y in zip(self._dir, (self._speed, self._speed)))
-        self._pos = tuple(x + y for x,y in zip(d, self._pos))
-        if self._pos[0] % 1 == 0 and self._pos[1] % 1 == 0:
-            x,y = self._pos
-            x,y = (int(x),int(y))
+        d = util.tuple_mult(self._dir, (self._speed, self._speed))
+        x1, y1 = util.tuple_add(d, self._pos)
+        x,y = self._pos = (round(x1,2), round(y1,2))
+        x,y = (int(x),int(y))
+        if (x,y) != self._body[0]:
             self._body.insert(0, (x,y))
-            if (x,y) != self._apple:
-                del self._body[-1]
-                if(x < 0 or x >= self.width or y < 0 or y >= self.height):
-                    self.gameOver()
-            else:
+            if (x,y) == self._apple:
+                self._growCount = self._growLen
+                self._apCount += 1
                 self.placeApple()
 
+            if self._growCount == 0:
+                del self._body[-1]
+            else:
+                self._growCount -= 1
+
+            if self._newDir:
+                self._dir = self._newDir
+                self._newDir = None
+
+            if(x < 0 or x >= self.width or y < 0 or y >= self.height) or (x,y) in self._body[1:]:
+                self.gameOver()
+            else:
+                if self._apCount >= 4:
+                    self.nextLevel()
+
     def placeApple(self):
-        x = randint(0, self.width-1)
-        y = randint(0, self.height-1)
+        while True:
+            x = randint(1, self.width-2)
+            y = randint(1, self.height-2)
+            if (x,y) not in self._body:
+                break
         self._apple = (x, y)
 
     def drawApple(self):
@@ -52,25 +90,7 @@ class Snake(BaseGameAnim):
         self._led.set(x, y, colors.Green)
 
     def step(self, amt=1):
-        if not self._gameOver:
-            if self._keys != self._lastKeys:
-                self._pos = self._body[0]
-                if self._keys.UP:
-                    self._dir = (0,-1)
-                if self._keys.DOWN:
-                    self._dir = (0,1)
-                if self._keys.RIGHT:
-                    self._dir = (1,0)
-                if self._keys.LEFT:
-                    self._dir = (-1,0)
-
-            self._lastKeys = self._keys
-
-            self.move()
-            self._led.all_off()
-            self.drawBody()
-            self.drawApple()
-        else:
+        if self._gameOver:
             self._led.all_off()
             self._led.drawText("GAME", self.width/2-11, self.height/2-8)
             self._led.drawText("OVER", self.width/2-11, self.height/2+1)
@@ -79,3 +99,31 @@ class Snake(BaseGameAnim):
                 self.resetBody()
                 self.placeApple()
                 self._gameOver = False
+        elif self._levelUp:
+            self._led.all_off()
+            self._led.drawText("LVL", self.width/2-8, self.height/2-8)
+            lvl = "{}".format(self._level)
+            w = len(lvl)*6
+            self._led.drawText(lvl, self.width/2-(w/2), self.height/2+1)
+            if self._keys.FIRE:#any(v > 0 for v in self._keys.itervalues()):
+                self._levelUp = False
+
+                self._lastKeys = None
+        else:
+            if self._keys != self._lastKeys:
+                self._pos = self._body[0]
+                if self._keys.UP and self._dir[1] == 0:
+                    self._newDir = self._directions[0]
+                if self._keys.DOWN and self._dir[1] == 0:
+                    self._newDir = self._directions[1]
+                if self._keys.RIGHT and self._dir[0] == 0:
+                    self._newDir = self._directions[2]
+                if self._keys.LEFT and self._dir[0] == 0:
+                    self._newDir = self._directions[3]
+
+                self._lastKeys = self._keys
+
+            self.move()
+            self._led.all_off()
+            self.drawBody()
+            self.drawApple()
