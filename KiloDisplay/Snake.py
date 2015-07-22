30fa821bcf728e0226399a0e75464f8d803cd3ba
adammhaile <adammhaile@gmail.com>
2015-07-22 18:25:35 -0400

    added lives  ad apple count

--- KiloDisplay/Snake.py
+++ KiloDisplay/Snake.py
@@ -10,8 +10,10 @@ class Snake(BaseGameAnim):
         self._growLen = 2
         self._speed = 0.3
         self._speedGrow = 0.1
+        self._lives = 4
         self._level = 1
         self._apCount = 0
+        self._apGoal = 6
         self._lastKeys = None
         self._apple = (-1, -1)
         self._gameOver = False
@@ -26,6 +28,18 @@ class Snake(BaseGameAnim):
         for b in self._body:
             x,y = b
             self._led.set(x, y, colors.Red)
+
+    def drawLives(self):
+        for i in range(self._lives):
+            self._led.set(self.width-1-i*2, 0, colors.Red)
+
+    def drawApplesLeft(self):
+        for i in range(self._apGoal - self._apCount):
+            self._led.set(i*2, 0, colors.Green)
+
+    def drawApple(self):
+        x,y = self._apple
+        self._led.set(x, y, colors.Green)
 
     def resetBody(self):
         dx, dy = self._dir = self._directions[0]#randint(0,3)]
@@ -42,6 +56,16 @@ class Snake(BaseGameAnim):
         self._levelUp = True
         self._level = 1
         self._speed = 0.3
+        self._lives = 4
+
+
+    def dead(self):
+        self._apCount = 0
+        self._lives -= 1
+        self._levelUp = True
+        if self._lives <= 0:
+            self.gameOver()
+
 
     def nextLevel(self):
         self._apCount = 0
@@ -71,10 +95,10 @@ class Snake(BaseGameAnim):
                 self._dir = self._newDir
                 self._newDir = None
 
-            if(x < 0 or x >= self.width or y < 0 or y >= self.height) or (x,y) in self._body[1:]:
-                self.gameOver()
+            if(x < 0 or x >= self.width or y < 1 or y >= self.height) or (x,y) in self._body[1:]:
+                self.dead()
             else:
-                if self._apCount >= 4:
+                if self._apCount >= self._apGoal:
                     self.nextLevel()
 
     def placeApple(self):
@@ -84,10 +108,6 @@ class Snake(BaseGameAnim):
             if (x,y) not in self._body:
                 break
         self._apple = (x, y)
-
-    def drawApple(self):
-        x,y = self._apple
-        self._led.set(x, y, colors.Green)
 
     def step(self, amt=1):
         if self._gameOver:
@@ -107,7 +127,8 @@ class Snake(BaseGameAnim):
             self._led.drawText(lvl, self.width/2-(w/2), self.height/2+1)
             if self._keys.FIRE:#any(v > 0 for v in self._keys.itervalues()):
                 self._levelUp = False
-
+                self.resetBody()
+                self.placeApple()
                 self._lastKeys = None
         else:
             if self._keys != self._lastKeys:
@@ -127,3 +148,5 @@ class Snake(BaseGameAnim):
             self._led.all_off()
             self.drawBody()
             self.drawApple()
+            self.drawApplesLeft()
+            self.drawLives()
---
+++ test.py
@@ -0,0 +1,27 @@
+from bibliopixel import LEDStrip, colors
+from bibliopixel.drivers.serial_driver import DriverSerial, LEDTYPE, ChannelOrder
+import time
+from bibliopixel import log
+log.setLogLevel(log.DEBUG)
+
+driver = DriverSerial(LEDTYPE.NEOPIXEL, 5, c_order=ChannelOrder.GRB)
+led = LEDStrip(driver, threadedUpdate=False)
+led.setMasterBrightness(64)
+
+try:
+    while True:
+        led.fill(colors.Red)
+        led.update()
+        time.sleep(0.5)
+        led.fill(colors.Green)
+        led.update()
+        time.sleep(0.5)
+        led.fill(colors.Blue)
+        led.update()
+        time.sleep(0.5)
+        led.fill(colors.White)
+        led.update()
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    led.all_off()
+    led.update()
