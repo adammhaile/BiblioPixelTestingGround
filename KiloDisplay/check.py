663088c3484b083c31ba4f0cbd770850acddcc80
adammhaile <adammhaile@gmail.com>
2015-08-03 18:07:00 -0400

    finished flappy bird

--- KiloDisplay/check.py
+++ KiloDisplay/check.py
@@ -1,4 +1,6 @@
 from kilo import *
+import sys
+sys.path.append("G:\GitHub")
 
 from bibliopixel.animation import MatrixCalibrationTest
 from matrix_animations import Bloom
@@ -6,6 +8,9 @@ from matrix_animations import Bloom
 from pad import pad
 from Snake import Snake
 from Tetris import Tetris
+from flappy import Flappy
+import BiblioPixelAnimations.matrix as matrix
+from matrix.GameOfLife import GameOfLife
 
 import bibliopixel.log as log
 log.setLogLevel(log.INFO)
@@ -16,7 +21,9 @@ if len(sys.argv) > 1:
 
 choices = {
     "snake":Snake(led, pad),
-    "tetris":Tetris(led, pad)
+    "tetris":Tetris(led, pad),
+    "flappy":Flappy(led, pad),
+    "gol": GameOfLife(led)
 }
 anim = choices[choice]
 try:
